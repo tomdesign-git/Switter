95ac29360d3db815e55fbcea4e5b5def0ad354a6
Tom Garnier <tomdesign-fr@gmail.com>
2020-03-26 19:41:37 +0100

    Création de la nouvelle application avec Svelte JS

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,46 @@
 <script>
 	export let name;
+
+	let logo = 'img/logo-horizontal.svg';
+	let message = "";
+	let messages = [];
+
+	// function updateMessage(event) {
+	// 	console.log(event.target.value);
+	// 	message = event.target.value;
+	// }
+
+	function saveMessage() {
+		const newMessage = {
+			id: Date.now(),
+			text: message,
+			author: 'Sam'
+		};
+		messages = [newMessage, ...messages];
+		message = "";
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<img src="{logo}"  class="logo" alt="{name}">
+	<br>
+	<textarea cols="50" rows="6" bind:value={message} placeholder="Ecrivez votre message..."/>
+	<br>
+	<button on:click={saveMessage}>Swit</button>
+	<div>
+		<h3>Aperçu</h3>
+		<div>
+		<textarea class="preview" cols="50" rows="5">{message}</textarea>
+		</div>
+	</div>
+	<div>
+		<h3>Messages</h3>
+		{#each messages as message}
+		<div>Par {message.author}</div>
+		<div>{message.text}</div>
+		<hr>
+		{/each}
+	</div>
+
+
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
